caff288c43b35007fbdd320d86c46d9711e68aca
chris-connoll-y <89544126+chris-connoll-y@users.noreply.github.com>
2021-11-10 09:26:14 -0800

    Added Basic Modifiers

--- AuctionListing.sol
+++ AuctionListing.sol
@@ -5,4 +5,18 @@ pragma solidity >=0.8.9;
 
 contract AuctionListing{
     
+    address public owner;
+    constructor (){
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+    
+    modifier validAddress(address _addr) {
+        require(_addr != address(0), "Not valid address");
+        _;
+    }
 }
